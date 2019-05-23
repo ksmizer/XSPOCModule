89faaf4277f5cd87c8a049aec3df043ab959bbaf
KeoniGarner <keoni_garner@yahoo.com>
2019-05-23 13:55:12 -0700

    Added context management to Gateway hook

--- lego-collector-module-gateway/src/main/java/pom/GatewayHook.java
+++ lego-collector-module-gateway/src/main/java/pom/GatewayHook.java
@@ -1,6 +1,7 @@
 package pom;
 
 import com.inductiveautomation.ignition.common.licensing.LicenseState;
+import com.inductiveautomation.ignition.common.sqltags.model.TagManager;
 import com.inductiveautomation.ignition.gateway.model.AbstractGatewayModuleHook;
 import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 import org.slf4j.Logger;
@@ -9,15 +10,18 @@ import org.slf4j.LoggerFactory;
 public class GatewayHook extends AbstractGatewayModuleHook {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-
+    private GatewayContext context;
+    private TagManager tagManager;
+    
     @Override
     public void setup(GatewayContext gatewayContext) {
-
+        context = gatewayContext;
+        tagManager = gatewayContext.getTagManager();
     }
 
     @Override
     public void startup(LicenseState licenseState) {
-
+        
     }
 
     @Override
@@ -25,4 +29,8 @@ public class GatewayHook extends AbstractGatewayModuleHook {
 
     }
 
+    @Override
+    public boolean isFreeModule() {
+        return true;
+    }
 }
