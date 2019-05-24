618b64d4e61f096489c176389dc8bca552004953
KeoniGarner <keoni_garner@yahoo.com>
2019-05-24 08:05:27 -0700

    Restarting Collaboration session #2
    Co-authored-by: Kyle Smizer-Muldoon <kylepmuldoon@gmail.com>
    Co-authored-by: Robert Pierucci <pierucci.robert@gmail.com>

---
+++ lego-collector-module-gateway/src/main/java/com/avadine/lego/collector/CollectorRunnable.java
@@ -0,0 +1,10 @@
+package com.avadine.lego.collector;
+
+public class CollectorRunnable implements Runnable {
+
+    public static void main(String[] args) {
+        Runnable runnable = new CollectorRunnable();
+        Thread collector = new Thread(runnable);
+        thread
+    }
+}
