24847c2b2c8d35a0fa8abd1a7232bf70398eb783
rpierucci <pierucci.robert@gmail.com>
2019-05-28 11:00:45 -0700

    Co-authored-by: Kyle Smizer-Muldoon <kylepmuldoon@gmail.com>

--- lego-collector-module-gateway/src/main/java/com/avadine/lego/collector/CollectorRunnable.java
+++ lego-collector-module-gateway/src/main/java/com/avadine/lego/collector/CollectorRunnable.java
@@ -1,16 +1,16 @@
 package com.avadine.lego.collector;
 
-import java.io.IOException;
+// import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
+// import java.util.Iterator;
 import java.util.Date; 
 import java.util.concurrent.ForkJoinPool;
 
 import com.inductiveautomation.ignition.common.model.values.QualifiedValue;
 import com.inductiveautomation.ignition.common.sqltags.model.TagPath;
 import com.inductiveautomation.ignition.common.sqltags.model.TagManager;
-import com.inductiveautomation.ignition.common.sqltags.parser.TagPathParser;
+// import com.inductiveautomation.ignition.common.sqltags.parser.TagPathParser;
 import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 
 import org.slf4j.Logger;
@@ -92,7 +92,7 @@ public class CollectorRunnable implements Runnable {
                 // Iterate through all points and check if they exist on gateway
 
                 ForkJoinPool pool = new ForkJoinPool();
-                PointTask task = new PointTask(tagManager, points, phantomPoints, tagPaths);
+                PointTask task = new PointTask(tagManager, logger, points, phantomPoints, tagPaths);
                 pool.invoke(task);
 
                 // Possible multi-threading opportunity
@@ -123,42 +123,42 @@ public class CollectorRunnable implements Runnable {
                 //     points.remove(point);
                 // }
                 
-                List<QualifiedValue> values = new ArrayList<QualifiedValue>();
-                List<QualifiedValue> badQualities = new ArrayList<QualifiedValue>();
+                // List<QualifiedValue> values = new ArrayList<QualifiedValue>();
+                // List<QualifiedValue> badQualities = new ArrayList<QualifiedValue>();
 
-                // Read all points for gateway
-                values = tagManager.read(tagPaths);
+                // // Read all points for gateway
+                // values = tagManager.read(tagPaths);
         
-                // Iterate through all points and check if they are good quality
-                for (int i = 0; i < values.size(); i++) { // Keoni: For loops should not use {length of list} - 1 unless we want to leave out the last value
-                    if (!values.get(i).getQuality().isGood()) {
-                        logger.warn("Collector Point: " + tagPaths.get(i) + " does not have Good quality. Skipping this tag.");
-                        badQualities.add(values.get(i));
-                        points.remove(i);
-                    }
-                }
-                
-                // Remove points that have bad qualities
-                // for (QualifiedValue badValue: badQualities) {
-                //     values.remove(badValue);
+                // // Iterate through all points and check if they are good quality
+                // for (int i = 0; i < values.size(); i++) { // Keoni: For loops should not use {length of list} - 1 unless we want to leave out the last value
+                //     if (!values.get(i).getQuality().isGood()) {
+                //         logger.warn("Collector Point: " + tagPaths.get(i) + " does not have Good quality. Skipping this tag.");
+                //         badQualities.add(values.get(i));
+                //         points.remove(i);
+                //     }
                 // }
-                values.removeAll(badQualities);
+                
+                // // Remove points that have bad qualities
+                // // for (QualifiedValue badValue: badQualities) {
+                // //     values.remove(badValue);
+                // // }
+                // values.removeAll(badQualities);
         
-                //call sproc
-                Date endTime = new Date();
-                int duration = Math.toIntExact((endTime.getTime()-startTime.getTime())/1000);
+                // //call sproc
+                // Date endTime = new Date();
+                // int duration = Math.toIntExact((endTime.getTime()-startTime.getTime())/1000);
         
-                for (int i = 0; i < points.size(); i++) {
-                    //construct PointToInsert objects
-                    logger.info("Creating point to insert with the ID: " + points.get(i).Id);
-                    PointToInsert insert = new PointToInsert();
-                    insert.Id = points.get(i).Id;
-                    insert.EffectiveDate = points.get(i).EffectiveDate;
-                    insert.PointValue = values.get(i).getValue().toString();
-                    insert.Duration = duration;
-                    logger.info("Inserting point with new value: " + insert.PointValue);
-                    // dc.insertPoint(insert);
-                }
+                // for (int i = 0; i < points.size(); i++) {
+                //     //construct PointToInsert objects
+                //     logger.info("Creating point to insert with the ID: " + points.get(i).Id);
+                //     PointToInsert insert = new PointToInsert();
+                //     insert.Id = points.get(i).Id;
+                //     insert.EffectiveDate = points.get(i).EffectiveDate;
+                //     insert.PointValue = values.get(i).getValue().toString();
+                //     insert.Duration = duration;
+                //     logger.info("Inserting point with new value: " + insert.PointValue);
+                //     // dc.insertPoint(insert);
+                // }
                 logger.info("Closing connection");
                 dc.closeConnection(); // need this so we don't have a memory leak
             } catch (NullPointerException e) {
--- lego-collector-module-gateway/src/main/java/com/avadine/lego/collector/PointTask.java
+++ lego-collector-module-gateway/src/main/java/com/avadine/lego/collector/PointTask.java
@@ -6,20 +6,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.RecursiveTask;
 
+import com.inductiveautomation.ignition.common.model.values.QualifiedValue;
 import com.inductiveautomation.ignition.common.sqltags.model.TagPath;
 import com.inductiveautomation.ignition.common.sqltags.model.TagManager;
 import com.inductiveautomation.ignition.common.sqltags.parser.TagPathParser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PointTask extends RecursiveTask {
 
     private List<Point> points;
-    private List<Point> leftPoints;
-    private List<Point> rightPoints;
     private List<Point> phantomPoints;
     private List<TagPath> tagPaths;
-    private Integer threshold;
+
     private TagManager tagManager;
     private TagPathParser parser = new TagPathParser();
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private Integer threshold;
 
     public PointTask(TagManager newTagManager, List<Point> pointList, List<Point> phantomPointList, List<TagPath> tagPathsList) {
         tagManager = newTagManager;
@@ -54,21 +59,50 @@ public class PointTask extends RecursiveTask {
                     }
                 }
             }
-        } else { //if not, split again
+            List<QualifiedValue> values = new ArrayList<QualifiedValue>();
+            List<QualifiedValue> badQualities = new ArrayList<QualifiedValue>();
+
+            // Read all points for gateway
+            values = tagManager.read(tagPaths);
+    
+            // Iterate through all points and check if they are good quality
+            for (int i = 0; i < values.size(); i++) { // Keoni: For loops should not use {length of list} - 1 unless we want to leave out the last value
+                if (!values.get(i).getQuality().isGood()) {
+                    logger.warn("Collector Point: " + tagPaths.get(i) + " does not have Good quality. Skipping this tag.");
+                    badQualities.add(values.get(i));
+                    points.remove(i);
+                }
+            }
+            
+            // Remove points that have bad qualities
+            // for (QualifiedValue badValue: badQualities) {
+            //     values.remove(badValue);
+            // }
+            values.removeAll(badQualities);
+    
+            //call sproc
+            Date endTime = new Date();
+            int duration = Math.toIntExact((endTime.getTime()-startTime.getTime())/1000);
+    
+            for (int i = 0; i < points.size(); i++) {
+                //construct PointToInsert objects
+                logger.info("Creating point to insert with the ID: " + points.get(i).Id);
+                PointToInsert insert = new PointToInsert();
+                insert.Id = points.get(i).Id;
+                insert.EffectiveDate = points.get(i).EffectiveDate;
+                insert.PointValue = values.get(i).getValue().toString();
+                insert.Duration = duration;
+                logger.info("Inserting point with new value: " + insert.PointValue);
+                // dc.insertPoint(insert);
+            }
+        } else { 
+            //if not, split again
             int length = points.size();
             int split = length/2;
             PointTask left = new PointTask(tagManager,points.subList(0,split),phantomPoints.subList(0,split),tagPaths.subList(0,split));
             PointTask right = new PointTask(tagManager,points.subList(split,length),phantomPoints.subList(split,length),tagPaths.subList(split,length));
             invokeAll(left,right);
         }
-
-
-       
-
-
-
-        
-
 
         return null;
         
